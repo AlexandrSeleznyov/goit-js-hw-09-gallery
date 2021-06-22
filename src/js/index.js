0ce1d2e0347ad4ae3901c385b8d139b445c1e37f
AlexandrSeleznyov <vuavso16@gmail.om>
2021-06-22 10:31:47 +0300

    DZ
    DZ

--- src/js/index.js
+++ src/js/index.js
@@ -4,72 +4,71 @@ import gallery from "./gallery-items.js";
 const galleryList = document.querySelector(".js-gallery");
 const lightbox = document.querySelector(".js-lightbox");
 const lightboxImage = document.querySelector(".lightbox__image");
-
-createGalleyList();
-galleryList.addEventListener("click", onGalleryClick);
-lightbox.addEventListener("click", onCloseImage);
-window.addEventListener("keydown", onKeyPress);
+const buttonClose = document.querySelector(".lightbox__button");
 
 
-function createGalleyList(){
-  const array = gallery.map((gallery, index) => {
-    return `
-    <li class="gallery__item">
+function  createGalleryList(){
+    return gallery.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
     <a
         class="gallery__link"
-        href="${gallery.original}"
+        href="${original}"
     >
         <img
         class="gallery__image"
-        src="${gallery.preview}"
-        data-source="${gallery.original}"
-        data-index= ${index}
-        alt="${gallery.description}"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
         />
     </a>
     </li>`;
-   }).join("");
-   galleryList.innerHTML=array;
-   console.log(galleryList);
-  };
+  }).join("");
+};
 
-function onGalleryClick(event){
-event.preventDefault();
-const dataSource = event.target.dataset.source;
-const idx = event.target.dataset.index;
-console.log("idx", idx);
+const arr = createGalleryList();
+galleryList.insertAdjacentHTML("afterbegin", arr);
 
-
-if (event.target.classList.value === "gallery__image"){
- lightbox.classList.add("is-open");
- lightboxImage.setAttribute("src", dataSource);
- lightboxImage.setAttribute("data-index", idx);
- 
- return idx;
-}
-}
-
-function onCloseImage(event){
-if ((event.target.classList.value === "lightbox__overlay")||(event.target.classList.value === "lightbox__button")){
-  event.preventDefault();
-  lightbox.classList.remove("is-open");
-  lightboxImage.setAttribute("src", "");
-}
-}
-
-function onKeyPress(event){
-  event.preventDefault();
-let indexEl = lightboxImage.dataset.index;
-  console.log("event index", lightboxImage.dataset.index);
-  if (event.key === "Escape"){
-    lightbox.classList.remove("is-open");
-  lightboxImage.setAttribute("src", "");
-  }
-  if (event.key === "ArrowLeft"){
-    if (indexEl>=0) {
-      indexEl -=1;
-      lightboxImage.setAttribute("src", gallery[indexEl].original);}
-    else {lightboxImage.setAttribute("src", gallery[gallery.length-1].original)}
+function onGalleryClick(e) {
+    e.preventDefault();
   
+    const dataSource = e.target.dataset.source;
+if (e.target.classList.value === "gallery__image") {
+    lightbox.classList.add("is-open");
+    lightboxImage.setAttribute("src", dataSource);
   }
 }
+
+
+function onCloseImage(e){
+    e.preventDefault();
+    console.log(e.target)
+    if (e.target.classList.value === "lightbox__button" || 
+    e.target.classList.value === "lightbox__overlay") {
+        lightbox.classList.remove("is-open");
+        lightboxImage.setAttribute("src", "");
+}
+}
+
+function onKeyPress(event) {
+    console.log(event);
+    let indexEl = gallery.findIndex(
+        (gallery) => gallery.original === lightboxImage.src
+      );
+    console.log(indexEl);
+    if (event.key === "Escape"){
+     lightbox.classList.remove("is-open");
+     lightboxImage.setAttribute("src", "");
+    }
+    if (event.key ==="ArrowLeft"){
+        indexEl === 0 ? (indexEl = gallery.length - 1) : (indexEl -= 1);
+     lightboxImage.setAttribute("src", gallery[indexEl].original)
+    }
+    if (event.key ==="ArrowRight"){
+       indexEl === gallery.length - 1 ? (indexEl = 0) : (indexEl += 1);
+     lightboxImage.setAttribute("src", gallery[indexEl].original)
+    }
+ }
+
+galleryList.addEventListener("click", onGalleryClick);
+window.addEventListener("keydown", onKeyPress);
+lightbox.addEventListener("click", onCloseImage);
